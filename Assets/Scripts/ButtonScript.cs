db09b15865f4d594da930a12e04e3062c2ae076a
Unknown <kelly.griffioen@gmail.com>
2019-05-20 22:40:14 +0200

    Start of menu

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class ButtonScript : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField]
+    private ScrollRectScript scrollRectScript;
+    [SerializeField]
+    private bool isDownButton;
+
+    public void OnPointerDown (PointerEventData eventData)
+    {
+        if (isDownButton)
+        {
+            scrollRectScript.ButtonDownIsPressed();
+        }
+        else
+        {
+            scrollRectScript.ButtonUpIsPressed();
+        }
+    }
+}
---
+++ Assets/Scripts/ScrollRectScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollRectScript : MonoBehaviour
+{
+    private ScrollRect scrollRect;
+    private bool mouseDown, buttonDown, buttonUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrollRect = GetComponent<ScrollRect>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mouseDown)
+        {
+            if (buttonDown)
+            {
+                ScrollDown();
+            }
+            else if (buttonUp)
+            {
+                ScrollUp();
+            }
+        }
+    }
+
+    public void ButtonDownIsPressed()
+    {
+        mouseDown = true;
+        buttonDown = true;
+    }
+
+    public void ButtonUpIsPressed()
+    {
+        mouseDown = true;
+        buttonUp = true;
+    }
+
+    private void ScrollDown()
+    {
+        mouseDown = false;
+        buttonDown = false;
+        scrollRect.verticalNormalizedPosition -= 0.5f;
+    }
+
+    private void ScrollUp()
+    {
+        mouseDown = false;
+        buttonUp = false;
+        scrollRect.verticalNormalizedPosition += 0.5f;
+    }
+}
