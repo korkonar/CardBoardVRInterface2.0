e6420432b0a0508e760dc2cf2b8a6c2553a73ae9
Unknown <kelly.griffioen@gmail.com>
2019-05-26 14:26:13 +0200

    Added timer and log file

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,9 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using UnityEditor;
 
 public class UIManager : MonoBehaviour
 {
+    private float timeOfAction;
+    public string action;
+    private bool timer = false;
+
+    private void Update()
+    {
+        if (timer)
+        {
+            timeOfAction += Time.deltaTime;
+        }
+    }
+
     //public void DisableFirstToSecond(Animator anim)
     //{
     //    anim.SetBool("FirstToSecond", false);
@@ -52,4 +66,34 @@ public class UIManager : MonoBehaviour
     {
         anim.SetBool("disableObject", true);
     }
+
+    public void StartTimer()
+    {
+        timeOfAction = 0.0f;
+        timer = true;
+    }
+
+    public void EndTimer()
+    {
+        timer = false;
+        WriteString(action, timeOfAction);
+    }
+
+    static void WriteString(string action, float timeOfAction)
+    {
+        string path = "Assets/Resources/log.txt";
+
+        //Write some text to the test.txt file
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine(action + " " + timeOfAction);
+        writer.Close();
+
+        //Re-import the file to update the reference in the editor
+        AssetDatabase.ImportAsset(path);
+        TextAsset asset = Resources.Load<TextAsset>("log");
+
+        //Print the text from the file
+        //Debug.Log(asset.text);
+    }
+
 }
